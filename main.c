5e384b4f341a4ed16ae2f16f06c920dcbce8bbe6
meryem achouri <maryamachouri50@gmail.com>
2024-05-15 10:16:14 +0100

    update

--- main.c
+++ main.c
@@ -5,102 +5,278 @@
 //les structures
 typedef struct User User;
 struct User {
-char name[50];
+char name[20];
 int numcompte;
-double solde;
+float solde;
 User *suiv;
 };
+int numcompte = 1;
 // les fonctions
-User *createAccount();
-User *modifyAccount(User *user);
-User *deleteAcount(User *user);
+//User *createAccount(User * user,char name[20],float solde);
+User *ajouterAccount(User * user,char name[20],float solde);
+void *modifyAccount(User * user,int numcompte);
+void *deleteAcount(User *user,int numcompte);
+void *afficherAccount(User *user);
+void *depotArgent(User *user,int numcompte);
+void *retirerArgent(User *user,int numcompte);
+void *transfererArgent(User *user,int numcompte1,int numcompte2);
+// contenu des fonction
+User* ajouterAccount(User *user, char name[20], float solde) {
+    User *Nouvuser = malloc(sizeof(User));    
+    strcpy(Nouvuser->name, name);
+    Nouvuser->solde = solde;
+     Nouvuser->numcompte = numcompte++;
+    Nouvuser->suiv = NULL;
 
+    if (user == NULL) {
+        return Nouvuser;
+    }
 
-// contenu des compte
-User *createAccount(){
+    User *courant = user;
+    while (courant->suiv != NULL) {
+        courant = courant->suiv;
+    }
+    courant->suiv = Nouvuser;
 
-User *user=malloc(sizeof(*user));
-    printf("saisir votre nom : ");
-          getchar();
-            fgets(user->name, sizeof(user->name), stdin); 
-                user->numcompte++;
-    printf("saisir votre solde initiale : ");
-          scanf("%f",&(user->solde));  
     return user;
 }
-User *modifyAccount(User *user){
-    int choix;
-     
-    printf("1 - modifiez votre nom : \n");
-    printf("2 -modifiez votre solde : \n");
-    printf("--------------------------------- \n");
-    while (choix >2 || choix <1){
-    scanf("%d",&choix);
-     if(choix==1){
-        printf("saisir votre nouveau nom : \n");
-            getchar();
-            fgets(user->name, sizeof(user->name), stdin);    }
-    else if(choix==2){
-      printf("saisir votre nouveau solde : ");
-         scanf("%f",user->solde);    }
+
+void *modifyAccount(User *user,int numcompte){
+    char name[50];
+      while (user!=NULL)
+      {
+        if(user->numcompte=numcompte){
+          printf("modifier votre nom : ");
+          getchar();
+          fgets(name, sizeof(name), stdin); 
+          strcpy(user->name, name);
+        }
+        user=user->suiv;
+      }
+      return user;
+  }
+ void *depotArgent(User *user, int numcompte) {
+    float nouvsolde;
+    printf("Combien d'argent voulez-vous déposer ? ");
+    scanf("%f", &nouvsolde);
+    while (user != NULL) {
+        if (user->numcompte == numcompte) {
+            user->solde = user->solde + nouvsolde;
+            return user;
+        }
+        user = user->suiv;
     }
+    printf("Compte avec numéro %d non trouvé.\n", numcompte);
+    return NULL;
+}
+void *retirerArgent(User *user, int numcompte) {
+    int accountFound=0;
+    float nouvsolde;
+
+    while (user != NULL) {
+        if (user->numcompte == numcompte) {
+            accountFound=1;
+            while (1) {
+               printf("Combien d'argent voulez-vous retirer ? ");
+                scanf("%f", &nouvsolde); 
+                if (nouvsolde <= user->solde) {
+                    user->solde = user->solde - nouvsolde;
+                    return user;
+                } else {
+                    printf("Le montant que vous essayez de retirer est superieur a votre solde. Veuillez reessayer : \n ");
+                }
+            }
+        }
+        user = user->suiv;
+    }
+
+  if (!accountFound) {
+        printf("Compte avec numéro %d non trouvé.\n", numcompte);
+    }    return NULL;
+
+
+}
+
+void *transfererArgent(User *user, int numcompte1, int numcompte2) {
+    float montant = 0;
    
+
+    if (user == NULL) {
+        printf("Compte avec numero %d non trouve.\n", numcompte1);
+        return NULL;
+    }
+     
+    printf("Saisir le montant a transferer depuis le compte %d vers le compte %d : ", numcompte1, numcompte2);
+    scanf("%f", &montant);
+    
+    if (montant >= user->solde) {
+        printf("Le montant que vous essayez de transferer est superieur au solde du compte %d. Veuillez reessayer.\n", numcompte1);
+        return NULL;
+    }
+       while (user != NULL) {
+        if (user->numcompte == numcompte1) {
+            user->solde -= montant;
+            break;
+        }
+        user = user->suiv;
+    }
+    while (user != NULL) {
+        if (user->numcompte == numcompte2) {
+            user->solde += montant;
+            break;
+        }
+        user = user->suiv;
+    }
+
+    if (user == NULL) {
+        printf("Compte avec numéro %d non trouvé.\n", numcompte2);
+        return NULL;
+    }
+    user = user->suiv;
+    
+
+    printf("Transfert de %.2f DH du compte %d vers le compte %d effectue avec succès.\n", montant, numcompte1, numcompte2);
+}
+
+  
+ void *deleteAcount(User *user, int numcompte) {
+    User *current = user;
+    User *prev = NULL;
+        while (current != NULL) {
+        if (current->numcompte == numcompte) {
+            if (prev == NULL) {
+                user = current->suiv;
+            } else {
+                prev->suiv = current->suiv;
+            }
+            free(current);
+            printf("le compte avec le numero %d supprime avec succes.\n", numcompte);
+            return user;
+        }
+        prev = current;
+        current = current->suiv;
+    }
+    printf("le compte avec le numero %d est introuvable.\n", numcompte);
     return user;
-  }
-  User *deleteAcount(User *user){
-        user=NULL;
-        printf("compte supprime avec succes");
-    return user;
-   
-  }
+}
+
+void *afficherAccount(User *user) {
+    User *courant = user;
+
+    printf("Liste des comptes actuelle : \n");
+    if (user == NULL) {
+        printf("Aucun compte cree.\n");
+    } else {
+        while (courant != NULL) {
+            printf("Le nom est : %s \n", courant->name);
+            printf("Numéro de compte : %d \n", courant->numcompte);
+            printf("Votre solde est : %.2f \n", courant->solde);
+            printf("*****************************\n");
+            courant = courant->suiv;
+        }
+    }
+}
 
 int main() {
 int choix;
-    User *user = NULL; // Initialisez user à NULL
-    
+int num;
+int num1,num2;
+char name[20];
+float solde;
+    User *user;
+    user=NULL; 
      do {
+        system("cls");
         printf("\n--- //// **gestion bancaire** //// ---\n");
-        printf("1. Creer un compte\n");
-        printf("2. Modifier le compte\n");
-        printf("3 - Supprimer lee compte \n");
-        printf("4 - ajouter");
-        printf("4. Exit\n");
+        printf("1 - Creer un compte\n");
+        printf("2 - Modifier le compte\n");
+        printf("3 - Deposer de l'argent sur votre compte \n");
+        printf("4 - Retirer de l'argent du votre compte \n");
+        printf("5 - transferer un montant vers un compte \n");
+        printf("6 - Supprimer le compte \n");
+        printf("7 - afficher mes comptes \n");
+        printf("8 - Exit\n");
         printf("Veuillez entrer votre choix : ");
         scanf("%d", &choix);
-
         switch (choix) {
             case 1:
-                user = createAccount();
+             printf("saisir votre nom : ");
+            getchar();
+              fgets(name, sizeof(name), stdin); 
+               printf("saisir votre solde initiale : ");
+              scanf("%f",&solde);  
+                user = ajouterAccount(user,name,solde);
+                while(getchar() != '\n');
+                  printf("Appuyez sur Entrée pour revenir au menu...");
+                  getchar();
                 break;
             case 2:
+         printf("saisir le numero du compte : \n");
+           scanf("%d",&num);
                 if (user != NULL) {
-                    user = modifyAccount(user);
+                     modifyAccount(user,num);
+            
                 } else {
                     printf("Erreur : Aucun compte n'a été créé.\n");
                 }
                 break;
-                case 3 :
-                if (user != NULL) {
-                    user = deleteAcount(user);
-                } 
+                  case 3 :
+                printf("saisir votre numero du compte : \n");
+                scanf("%d",&num);
+                  depotArgent(user,num);
+                  while (getchar() != '\n');
+                  printf("Appuyez sur Entrée pour revenir au menu...");
+                  getchar();
                 break;
-            case 4:
+                 case 4 :
+                printf("saisir votre numero du compte : \n");
+                scanf("%d",&num);
+                  retirerArgent(user,num);
+                  while (getchar() != '\n');
+                  printf("Appuyez sur Entrée pour revenir au menu...");
+                  getchar();
+                break;
+                 case 5 :
+                printf("saisir votre numero du compte : \n");
+                scanf("%d",&num1);
+                 printf("saisir le numero du compte destinataire : \n");
+                scanf("%d",&num2);
+                  transfererArgent(user,num1,num2);
+                  while (getchar() != '\n');
+                  printf("Appuyez sur Entrée pour revenir au menu...");
+                  getchar();
+                break;
+                case 6 :
+                printf("saisir votre numero du compte : \n");
+                scanf("%d",&num);
+                if (user != NULL) {
+                  user =   deleteAcount(user,num);
+                  while (getchar() != '\n');
+                  printf("Appuyez sur Entrée pour revenir au menu...");
+                  getchar();
+                }
+                
+                break;
+                 case 7 :
+                if (user != NULL) {
+                 afficherAccount(user);
+                  while (getchar() != '\n');
+                  printf("Appuyez sur Entrée pour revenir au menu...");
+                  getchar();
+                } 
+                
+                break;
+            case 8:
                 printf("Exiting...\n");
                 break;
             default:
                 printf("Choix invalide. Veuillez réessayer.\n");
         }
-    } while (choix!=3);
-    {
-        /* code */
-    }
-    
-
+    } while (choix!=8);
     // Libérer la mémoire allouée pour l'utilisateur avant de quitter le programme
     if (user != NULL) {
         free(user);
     }
-
     return 0;
 }
  /*while(user!=NULL){
--- test.c
+++ test.c
@@ -1,137 +1,228 @@
 #include <stdio.h>
-#include<stdbool.h>
-#include<string.h>
-// Structure qui represente le compte bancaire 
-typedef struct Account Account;
-struct Account {
-    char nom[50];
-    int num_acc;
-    float solde;
-};
+#include <stdbool.h>
+#include <stdlib.h>
+
+// Système de gestion bancaire
+
 void clearInputBuffer()
 {
   int c;
   while ((c = getchar()) != '\n' && c != EOF)
     ;
 }
- Account creelecompte() {
-    Account new_account;
 
-    printf("veuillez enter votre nom : ");
-   // scanf("%s", new_account.nom);
-   getchar();
-   fgets(new_account.nom , sizeof(new_account.nom),stdin);
+int operationsOfOurBank(void)
+{
+  int serviceNumber;
+  system("cls");
+  printf("Créer votre compte en tapant le numéro                    : 0\n");
+  printf("Voir les informations de votre compte en tapant le numéro : 1\n");
+  printf("Modifier votre solde en tapant le numéro                  : 2\n");
+  printf("Transférer un montant vers un autre compte                : 3\n");
+  printf("Voir tous les comptes existants dans la base de données   : 4\n");
 
-
-    printf("veuillez enter votre numero de compte : ");
-    scanf("%d", &new_account.num_acc);
-
-    printf("veuillez enter votre solde initial : ");
-    scanf("%f", &new_account.solde);
-
-    return new_account;
-}
-void checksolde(Account *account ) {
-    printf("le solde du compte : %.2f\n", account->solde);
-}
-void supprimercompte(Account *account,int *pos) {
-    int i;
-    int taille =strlen(account);
-    for(i=0;i<taille;i++){
-        account[i]=account[i+1];
-        taille--;
+  bool isValid = false;
+  while (!isValid)
+  {
+    printf("Entrez un numéro pour débuter une opération : ");
+    scanf("%d", &serviceNumber);
+    clearInputBuffer();
+    if (serviceNumber == 0 ||
+        serviceNumber == 1 ||
+        serviceNumber == 2 ||
+        serviceNumber == 3 ||
+        serviceNumber == 4)
+    {
+      isValid = true;
+      return serviceNumber;
     }
-    printf("compte supprimer avec succes.\n");
+    else
+    {
+      while (getchar() != '\n')
+        ;
+      system("cls");
+      printf("Veuillez entrer un nombre entre 0 et 4.\n");
+    }
+  }
 }
-void modifiercompte( Account *account) {
-    printf("veuillez enter votre nouveau nom : ");
-    scanf("%s", account->nom);
 
-    printf("veuillez enter votre nouveau solde : ");
-    scanf("%f", &account->solde);
+struct user
+{
+  char fullName[50];
+  int acountNumber;
+  float balance;
+};
 
-    printf("votre compte a ete bien modifier .\n");
-}
-int main() {
-       struct Account accounts[100]; 
-    int num_accounts = 0;
+int main()
+{
 
-    int choix;
-    do {
-        printf("\n--- //// **gestion bancaire** //// ---\n");
-        printf("1. Cree un compte \n");
-        printf("2. verifier le solde \n");
-        printf("3. supprimer le compte \n");
-        printf("4. Modifier le compte \n");
-        printf("5. Exit\n");
-        printf("veuillez enter votre choix : ");
-        scanf("%d", &choix);
+  struct user newUser[50];
+  for (int i = 0; i < 50; i++)
+  {
+    newUser[i].acountNumber = 0;
+  }
 
- switch (choix) {
-            case 1:
-                accounts[num_accounts++] = creelecompte();
-                break;
-            case 2:
-                if (num_accounts > 0) {
-                    int acc_num;
-                    printf("veuillez enter votre numero de compte : ");
-                    scanf("%d", &acc_num);
-                    for (int i = 0; i < num_accounts; i++) {
-                        if (accounts[i].num_acc == acc_num) {
-                            checksolde(&accounts[i],pos);
-                            break;
-                        }
-                        if (i == num_accounts - 1)
-                            printf("compte anvalid.\n");
-                    }
-            case 3:
-                if (num_accounts > 0) {
-                    int acc_num;
-                    printf("veuillez enter le numero de compte a supprimer : ");
-                    scanf("%d", &acc_num);
-                    for (int i = 0; i < num_accounts; i++) {
-                        if (accounts[i].num_acc == acc_num ) {
-                            supprimercompte(&accounts[i]);
-                            break;
-                        }
-                        if (i == num_accounts - 1)
-                            printf("compte non valid .\n");
-                    }
-                } else {
+  int lastAcountNumber = 0;
+  int tmpAcountNumber;
+  char tmpFullName[50];
+  system("cls");
+  int serviceNumber;
+  while (true)
+  {
+    serviceNumber = operationsOfOurBank();
+    switch (serviceNumber)
+    {
+    case 0:
+      system("cls");
+      printf("Votre numéro est : %d \n", serviceNumber);
+      printf("Pour créer votre compte, il faut quelques informations sur vous.\n");
+      printf("Votre nom et prénom : ");
+      fgets(tmpFullName, sizeof(tmpFullName), stdin);
+      clearInputBuffer();
+      lastAcountNumber++;
+      newUser[lastAcountNumber].balance = 0;
+      newUser[lastAcountNumber].acountNumber = lastAcountNumber;
+      strcpy(newUser[lastAcountNumber].fullName, tmpFullName);
+      printf("Votre nom : %s\n", newUser[lastAcountNumber].fullName);
+      printf("Votre numéro de compte : %d\n", newUser[lastAcountNumber].acountNumber);
+      printf("Votre solde : %d\n", (int)newUser[lastAcountNumber].balance); // Correction pour l'affichage du solde
+      printf("Pour revenir à la page principale, tapez 0 : ");
 
-                    printf("Aucun compte disponible.\n");
-                }
-                break;
-            case 4:
-                if (num_accounts > 0) {
-                    int acc_num;
-                    printf("veuillez enter le numero de compte a modifier : ");
-                    scanf("%d", &acc_num);
-                    for (int i = 0; i < num_accounts; i++) {
-                        if (accounts[i].num_acc == acc_num ) {
-                            modifiercompte(&accounts[i]);
-                            break;
-                        }
-                        if (i == num_accounts - 1)
-                            printf("compte non valid .\n");
-                    }
-                } else {
-                    printf("Aucun compte disponible.\n");
-                }
-                break;
-            case 5:
-                printf("Exiting...\n");
-                break;
-                default:
-                printf("Choix invalide. Veuillez réessayer.\n");
+      int exitVar;
+      scanf("%d", &exitVar);
+      clearInputBuffer();
+      if (exitVar == 0)
+      {
+        break;
+      }
+      break;
+    case 1:
+      system("cls");
+      printf("Le numéro de votre opération est : %d\n", serviceNumber);
+      printf("Pour voir vos informations, tapez votre numéro de compte : ");
+
+      scanf("%d", &tmpAcountNumber);
+      clearInputBuffer();
+      for (int i = 0; i < 50; i++)
+      {
+        if (newUser[i].acountNumber == tmpAcountNumber)
+        {
+          printf("Votre nom : %s\n", newUser[i].fullName);
+          printf("Votre numéro de compte : %d\n", newUser[i].acountNumber);
+          printf("Votre solde : %d\n", (int)newUser[i].balance); // Correction pour l'affichage du solde
+          break;
         }
-                }
-       for(int i=0;i<num_accounts;i++){
-        printf("%s \n",accounts[i].nom);
-         printf("%d \n",accounts[i].num_acc);
-         printf("%f",accounts[i].solde);
-       }
+      }
+      printf("Pour revenir à la page principale, tapez 0 : ");
+      scanf("%d", &exitVar);
+      clearInputBuffer();
+      if (exitVar == 0)
+      {
+        break;
+      }
+      break;
+    case 2:
+      system("cls");
+      printf("Le numéro de votre opération est : %d\n", serviceNumber);
+      printf("Pour modifier votre solde, entrez votre numéro de compte : ");
+      scanf("%d", &tmpAcountNumber);
+      clearInputBuffer();
+      for (int i = 0; i < 50; i++)
+      {
+        if (newUser[i].acountNumber == tmpAcountNumber)
+        {
+          printf("Votre nom : %s\n", newUser[i].fullName);
+          printf("Votre numéro de compte : %d\n", newUser[i].acountNumber);
+          printf("Votre solde : %d\n", (int)newUser[i].balance); // Correction pour l'affichage du solde
+          break;
+        }
+      }
+      int nouv;
+      for (int i = 0; i < 50; i++)
+      {
+        if (newUser[i].acountNumber == tmpAcountNumber)
+        {
+            
+          printf("Entrez votre nouveau solde : ");
+          scanf("%d", &nouv);
+          newUser[i].balance=nouv;
+          printf("Nouveau solde : %f\n",newUser[i].balance); 
+          break;
+        }
+      }
+
+      printf("Pour revenir à la page principale, tapez 0 : ");
+      scanf("%d", &exitVar);
+      clearInputBuffer();
+      if (exitVar == 0)
+      {
+        break;
+      }
+      break;
+    case 3:
+      system("cls");
+      int acountNumber_post;
+      int acountNumber_get;
+      int moneyValue;
+      printf("Le numéro de votre opération est : %d\n", serviceNumber);
+      printf("Vous êtes ici pour transférer de l'argent vers un autre compte.\n");
+      printf("Veuillez entrer le numéro de votre compte : ");
+      scanf("%d", &acountNumber_post);
+      clearInputBuffer();
+      printf("Entrez le numéro du compte destinataire : ");
+      scanf("%d", &acountNumber_get);
+      clearInputBuffer();
+      printf("Entrez le montant à transférer : ");
+      scanf("%d", &moneyValue);
+      clearInputBuffer();
+
+      for (int i = 0; i < 50; i++)
+      {
+        if (newUser[i].acountNumber == acountNumber_post)
+        {
+          newUser[i].balance -= moneyValue;
+          break;
+        }
+      }
+
+      for (int i = 0; i < 50; i++)
+      {
+        if (newUser[i].acountNumber == acountNumber_get)
+        {
+          newUser[i].balance += moneyValue;
+          break;
+        }
+      }
+      printf("Pour revenir à la page principale, tapez 0 : ");
+      scanf("%d", &exitVar);
+      clearInputBuffer();
+      if (exitVar == 0)
+      {
+        break;
+      }
+      break;
+    case 4:
+      for (int i = 0; i < 50; i++)
+      {
+        if (newUser[i].acountNumber != 0)
+        {
+          printf("Votre nom : %s\n", newUser[i].fullName);
+          printf("Votre numéro de compte : %d\n", newUser[i].acountNumber);
+          printf("Votre solde : %d\n", (int)newUser[i].balance); // Correction pour l'affichage du solde
+        }
+      }
+      printf("Pour revenir à la page principale, tapez 0 : ");
+      scanf("%d", &exitVar);
+      clearInputBuffer();
+      if (exitVar == 0)
+      {
+        break;
+      }
+      break;
+    default:
+      serviceNumber = operationsOfOurBank();
+      break;
     }
-    while (choix != 5);
-    return 0;
+  }
+  return 0;
 }
